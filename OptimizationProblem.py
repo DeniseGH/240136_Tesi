40b68da4730ec635b363510fa37190533bf29668
Denise Tampieri <Denisetampieri@icloud.com>
2024-08-27 22:13:32 +0200

    first commit

---
+++ OptimizationProblem.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import random
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import networkx as nx
+
+from GraphicalModel import CausalGraphicalModel
+from CausalModel import StructuralCausalModel
+from CausalModel import CausalAssignmentModel
+
+TRUE_THETA_MAX = 5
+TRUE_THETA_MIN = -5
+
+# Here I changed the theta definition to be a vector instead of a matrix. Basically, since most of the entries
+# of the matrix are zero, we can just consider a "flatter" representation.
+def generate_scm(thetas):
+    generator = np.random.default_rng(2024)
+    scm = StructuralCausalModel({
+        "x1": lambda n_samples, thetas=thetas: generator.normal(loc=0, scale=1, size=n_samples),
+        "x2": lambda x1, n_samples, thetas=thetas: thetas[0] * x1 + generator.normal(loc=0, scale=1, size=n_samples),
+        "x3": lambda x2, x1, n_samples, thetas=thetas: thetas[1] * x1 + thetas[2] * x2 + generator.normal(loc=0,
+                                                                                                          scale=1,
+                                                                                                      size=n_samples),
+    })
+    return scm
+
+
+# %%
+# I've changed this function to perform a "soft intervention" instead.
+# Basically, given the intervention value v, we just add it to the variable (e.g., X = X + v).
+# Previously, we were using a "hard intervention", which basically overwrite the value of the variable (e.g, X = v)
+# by removing the corresponding parents edges.
+# I had to change the StructuralCausalModel class (the sample() function) to enable soft interventions.
+def intervened_data(scm, intervention, intervention_type):
+    node, value = intervention
+    # scm_do = scm.do(node)
+    # We perform here the sampling of 100 instances to compute the expected value of the intervention E[X | do(X=X+v]
+    ds_do = scm.sample(n_samples=100, set_values={node: np.full(100, value)}, type_of_intervention=intervention_type)
+    return ds_do
+
+
+
+# %%
+# The prior is taken from zeus example. Basically, a particle is valid if and only
+# if it lies between -5 and 5. If a particle k lies there, then P(\theta=k) = 1.
+# Thus, the logprobability is 0, since np.log(1) = 0. However, if the particle
+# is not in the correct range, the logprob is -infinity, since np.log(0) is undefined.
+def log_prior(theta, a, b):
+    if np.all(theta > a) and np.all(theta < b):
+        return 0.0
+    else:
+        return -np.inf
+
+
+# %%
+# We consider a "hard" version of the likelihood function. Basically, given the user preferences
+# we assign a positive probability only to those particles which matches **all** the ground truth choices.
+# Basically, in the code below, estimated_result and ground_truth_choice must match always.
+def likelihood(interventions, values_real_scm, estimated_thetas, epsilon=0.1):
+    counter = []
+    for intervention, scm_do_real in zip(interventions, values_real_scm):
+
+        # generate the scm with the estimated thetas
+        scm_temp = generate_scm(estimated_thetas)
+        scm_do = intervened_data(scm_temp, intervention, "soft")
+
+        node, value = intervention
+
+        delta_2 = scm_do["x3"].values - scm_do_real["x3"].values
+
+        result = True
+        # If the node is "x1", also calculate the difference for x2
+        if node == "x1":
+            delta_1 = np.abs(scm_do["x2"].values - scm_do_real["x2"].values)
+
+            # Check if all values in both delta_1 and delta_2 are less than 0.1
+            if not (np.all(delta_1 < epsilon) and np.all(delta_2 < epsilon)):
+                result = False
+
+        else:
+            # Check if all values in delta_2 are less than 0.1
+            if not np.all(delta_2 < epsilon):
+                result = False
+
+        counter.append(result)
+
+    # Calculate the likelihood based on the consistency count
+    likelihood_value = np.all(counter)
+    # print("likelihood_value", likelihood_value)
+    return likelihood_value
+
+
+# We combine the likelihood and the prior in the logposterior.
+def log_posterior_new(thetas, interventions, values_real_scm):
+    log_lk = likelihood(interventions, values_real_scm, thetas)
+    log_lk = np.log(log_lk) if log_lk > 0 else -np.inf
+    return log_lk + log_prior(thetas, TRUE_THETA_MIN, TRUE_THETA_MAX)
---
+++ main.py
@@ -0,0 +1,147 @@
+
+# %% [markdown]
+# # Application
+
+# %%
+import matplotlib.pyplot as plt
+import random
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import zeus
+import OptimizationProblem
+import networkx as nx
+
+
+from GraphicalModel import CausalGraphicalModel
+from CausalModel import StructuralCausalModel
+from CausalModel import CausalAssignmentModel
+
+
+TRUE_THETAS = [-2, 1, 4]
+TRUE_THETA_MAX = 5
+TRUE_THETA_MIN = -5
+
+
+# Set some seed for reproducibility
+np.random.seed(2024)
+
+# %%
+interventions_num = 10
+
+# Basically, we first sample the variable we are going to intervene upon (e.g., x1 or x2)
+# then, we sample the value we want to add to that variable. We sample the value between -1 and 1.
+interventions = []
+for i in range(interventions_num):
+    current_comparison = []
+    node = str(np.random.choice(["x1", "x2"], 1)[0])  # Convert to Python string
+    value = np.random.rand()
+    # current_comparison.append([node, value])
+    interventions.append([node, value])
+
+
+# Interventions are simply a collection of group of tuples:
+# [(node, value)]
+print("intervention")
+print(interventions)
+
+# Generate real values of the user after the intervention and store them in a list
+simulation_scm = OptimizationProblem.generate_scm(TRUE_THETAS)
+values_real_scm = []
+for intervention in (interventions):
+    scm_do_real = OptimizationProblem.intervened_data(simulation_scm, intervention, "soft")
+    values_real_scm.append(scm_do_real)
+
+print("values_real_scm[0]")
+print(values_real_scm[0])
+
+# %%
+ndim = 3  # Number of non-zero parameters of the SCM matrix. In our case, it is 3
+nwalkers = 50  # Number of particles (they call them walkers)
+nsteps = 100  # Number of steps/iterations.
+
+# Generally, the larger nwalkers and nsteps are, the longer the procedure will take.
+# In Colab, everything runs on a single core, so it is dead slow.
+
+# Increasing also the number of pairwise comparison makes everything even slower.
+# I guess there are ways to speed up the likelihood function, but let's stick with this one for now.
+
+# %%
+# We need to sample an initial particle population. In order to do so, we need to be
+# sure the initial particles are **valid** (e.g., they have a finite logprobability)
+# Thus, we sample random particles from a uniform distribution and we compute the log
+# probability. We only keep such particles that have a finite logprob.
+#
+# NOTE: if we increase the number of interventions, it would take much more time
+# to find suitable particles/walkers!!!
+print("start....")
+start = []
+while (len(start) < nwalkers):
+    tmp = np.random.uniform(TRUE_THETA_MIN, TRUE_THETA_MAX, ndim)
+    if np.isfinite(OptimizationProblem.log_posterior_new(tmp, interventions, values_real_scm)):
+        print(f"FOUND {len(start) + 1}/{nwalkers}")
+        start.append(tmp)
+
+
+# %%
+print("sampler....")
+sampler = zeus.EnsembleSampler(nwalkers, ndim, OptimizationProblem.log_posterior_new, args=[interventions, values_real_scm],
+                               verbose=True,
+                               light_mode=True)  # Initialise the sampler
+sampler.run_mcmc(start, nsteps)  # Run sampling
+sampler.summary  # Print summary diagnostics
+
+# %%
+# Get the samples
+samples = sampler.get_chain()
+
+# Plot the walker trajectories for the first parameter of the 10
+fig, ax = plt.subplots(3, 1, figsize=(16, 12))
+
+# For each node, plot the convergence of the particles
+for id in range(3):
+    ax[id].plot(samples[:, :, id], alpha=0.5)
+    ax[id].set_title(f"Convergence for $X_{id + 1}$")
+    ax[id].set_ylim(TRUE_THETA_MIN, TRUE_THETA_MAX)
+plt.show()
+
+# %%
+# Given the samplers we compute the estimated theta and we compare everything with the correct distribution.
+# In theory, if the procedure converged correctly, the plots should be very similar.
+
+# We discard the first 50% of the chain particles, since that phase it is usually called burn-in.
+chain = sampler.get_chain(flat=True, discard=0.5)
+
+# %%
+# We compute the average to compute the thetas
+estimated_theta = np.mean(chain, axis=0)
+
+# %%
+print("estimated_theta")
+print(estimated_theta)
+
+# %%
+print("TRUE_THETAS")
+print(TRUE_THETAS)
+
+# %%
+# Sample elements from the estimated SCM
+scm_estimated = OptimizationProblem.generate_scm(estimated_theta)
+estm_data = scm_estimated.sample(1000)
+
+# Get the true data
+true_data = OptimizationProblem.simulation_scm.sample(1000)
+
+# %%
+# Plot the various distributions.
+# We can see how they are basically dissimilar. Moreover, there is an error propagation effect.
+# Since the estimation of X1 is partially correct (e.g., theta = -2.4 instead of 4), then all
+# the subsequent X2 and X3 are poorly estimated.
+# Interestingly, the preference tells us a different story.
+
+fig, ax = plt.subplots(1, 3, figsize=(16, 4))
+for id in range(3):
+    sns.kdeplot(estm_data[f"x{id + 1}"], ax=ax[id])
+    sns.kdeplot(true_data[f"x{id + 1}"], ls='--', ax=ax[id])
+    ax[id].legend([f'$X_{id + 1}$', 'Truth']);
+
